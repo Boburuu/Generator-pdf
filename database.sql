193a3090243f90bf08e456793634348ffaec6c6b
Boburuu <camille.disera.30@gmail.com>
2023-12-20 10:58:58 +0100

    update table database.sql

--- database.sql
+++ database.sql
@@ -1,17 +1,11 @@
-
-CREATE TABLE `default`.`Entity`  (
+CREATE TABLE `default`.`Assoc`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `token` varchar(255) NOT NULL,
   `data_pdf` json NOT NULL,
+  `generate` int  NULL,
+  `generate_at` TIMESTAMP  NULL,
+  `signature` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE`(`id`)
-);
-
-CREATE TABLE `default`.`cerfa_pdf`  (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `number` int NOT NULL,
-  `entity_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_entity_id` FOREIGN KEY (`entity_id`) REFERENCES `Entity`(`id`)
+  UNIQUE INDEX `id_UNIQUE` (`id`)
 );
